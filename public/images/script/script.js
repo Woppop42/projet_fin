d5ac5496b43fd10f4009cbd68a28019f0810862a
Gabriel <gokba27@gmail.com>
2023-08-29 16:01:19 +0200

    balise de retour

---
+++ public/images/script/script.js
@@ -0,0 +1,4 @@
+document.getElementById("retour").addEventListener("click", function() {
+    // Utilise la méthode history.back() pour retourner à la page précédente
+    window.history.back();
+});
