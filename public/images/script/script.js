d29cc7f4fdeeeb1ffb4684a039584fd786bef762
Gabriel <gokba27@gmail.com>
2023-08-29 16:31:53 +0200

    on avance bien

--- public/images/script/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("retour").addEventListener("click", function() {
-    // Utilise la méthode history.back() pour retourner à la page précédente
-    window.history.back();
-});
